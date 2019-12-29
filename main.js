3196d1ce87b5f9f3b86499e2e5a8ebf9753421c8
Bryan <bbcashmoney@gmail.com>
2019-12-29 03:34:09 +0200

    Ran into a problem with server

--- main.js
+++ main.js
@@ -17,17 +17,17 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
-// app.get('/', (req, res) => {
-//     res.render('index', {layout: false});
-// });
+app.get('/', (req, res) => {
+    res.render('index', {layout: false});
+});
 
 app.get('/contact', (req, res) => {
     res.render('contact', {layout: false});
 });
 
-// app.get('/portfolio', (req, res) => {
-//     res.render('portfolio', {layout: false});
-// });
+app.get('/portfolio', (req, res) => {
+    res.render('portfolio', {layout: false});
+});
 
 app.post('/send', (req, res) => {
     const output = `
@@ -40,38 +40,40 @@ app.post('/send', (req, res) => {
     <h3>Message</h3>
     <p>Message: ${req.body.message}</p>`
 
-    // create reusable transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass // generated ethereal password
-    },
-    tls:{
-        rejectUnauthorized = true
-    }
-});
+    console.log(output);
 
-  // send mail with defined transport object
-let info = await transporter.sendMail({
-    from: '"Nodemailer" <foo@example.com>', // sender address
-    to: "bbcashmoney@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: output // html body
-});
+//     // create reusable transporter object using the default SMTP transport
+// let transporter = nodemailer.createTransport({
+//     host: "smtp.ethereal.email",
+//     port: 587,
+//     secure: false, // true for 465, false for other ports
+//     auth: {
+//       user: testAccount.user, // generated ethereal user
+//       pass: testAccount.pass // generated ethereal password
+//     },
+//     tls:{
+//         rejectUnauthorized = true
+//     }
+// });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+//   // send mail with defined transport object
+// let info = await transporter.sendMail({
+//     from: '"Nodemailer" <foo@example.com>', // sender address
+//     to: "bbcashmoney@gmail.com", // list of receivers
+//     subject: "Hello ✔", // Subject line
+//     text: "Hello world?", // plain text body
+//     html: output // html body
+// });
 
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-  res.render('contact', {msg:'Email has been sent!'});
+//   console.log("Message sent: %s", info.messageId);
+//   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-main().catch(console.error);
+//   // Preview only available when sending through an Ethereal account
+//   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+//   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+//   res.render('contact', {msg:'Email has been sent!'});
+
+// main().catch(console.error);
 });
 
 app.listen(3000, () => console.log('Sever started...'));
